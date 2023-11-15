cdb4fec09f3cc1e0d0d4a1bea60ce4dcfe17c280
younes0311 <144245293+younes0311@users.noreply.github.com>
2023-11-15 12:06:10 +0100

    Update style.js

--- style.js
+++ style.js
@@ -31,7 +31,7 @@ let bigfoto1 = setTimeout(function(){
     foto3.style.display='none';
     foto1.style.animation='none';
     foto1.style.transform='translateX(600px)';
-
+    foto1.style.height='500px';
 },5800)
 
 
